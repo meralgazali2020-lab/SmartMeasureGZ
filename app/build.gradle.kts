db14d9ea4cca2dab33f719721a9e16f54c5e80e9
meralgazali2020-lab <meralgazali2020@gmail.com>
2026-08-20 01:49:11 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,12 @@ android {
 }
 
 dependencies {
+
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.2.0")
+
+    // قراءة النص والمقاسات من الصور
+    implementation("com.google.mlkit:text-recognition:16.0.1")
 }
