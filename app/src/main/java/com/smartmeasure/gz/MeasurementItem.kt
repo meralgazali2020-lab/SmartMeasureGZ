df72189e4864e19b8a9cb14089de99070984c4a8
meralgazali2020-lab <meralgazali2020@gmail.com>
2026-08-20 03:43:44 +0300

    Add operationNumber to MeasurementItem
    Added operationNumber property to MeasurementItem data class.

--- app/src/main/java/com/smartmeasure/gz/MeasurementItem.kt
+++ app/src/main/java/com/smartmeasure/gz/MeasurementItem.kt
@@ -1,10 +1,20 @@
 package com.smartmeasure.gz
 
 data class MeasurementItem(
+
     var length: Double,
+
     var width: Double,
+
     var quantity: Int = 1,
+
     var unit: String = "سم",
+
     var adjustedLength: Double = length,
-    var adjustedWidth: Double = width
+
+    var adjustedWidth: Double = width,
+
+    // رقم العملية داخل المشروع
+    // مثال: 1، 2، 3، 4...
+    var operationNumber: Int = 0
 )
